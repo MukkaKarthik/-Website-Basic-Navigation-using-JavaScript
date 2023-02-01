21bd2640d65184f67b4bf85d7c7b2121d8cac1a4
MukkaKarthik <124203295+MukkaKarthik@users.noreply.github.com>
2023-02-02 01:59:07 +0530

    Add files via upload

---
+++ WebsiteNavigation.js
@@ -0,0 +1,9 @@
+function GoogleImagesWebsite()
+{
+    window.location = "https://www.google.com/search?q=images&sxsrf=ALeKk02oaP7pvdNiabxNHGaLB8hNY4EU8g:1626783953150&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjC85y90vHxAhXZyjgGHQTbA3AQ_AUoAXoECAEQAw&biw=1536&bih=754";
+}
+
+function NewWebsite()
+{
+    window.location = "NewWebsite.html";
+}
